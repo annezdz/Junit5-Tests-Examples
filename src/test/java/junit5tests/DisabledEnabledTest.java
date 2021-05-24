ea4fdb1d0055b9e1a2d5e155982a5b538cd03656
annezdz <annezdz@hotmail.com>
2021-05-24 20:47:51 -0300

    Created DisabledEnabledTest

---
+++ src/test/java/junit5tests/DisabledEnabledTest.java
@@ -0,0 +1,44 @@
+package junit5tests;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class DisabledEnabledTest {
+
+    @Test
+    @Disabled(value = "Disabled for demo of @Disabled")
+    void firstTest(){
+        System.out.println("This is the first test method");
+    }
+
+    @Test
+    @DisabledOnOs(value = OS.WINDOWS, disabledReason = "Disabled for demo of @DisabledOnOs")
+    void secondTest(){
+        System.out.println("This is the second test method");
+    }
+
+    @Test
+    @DisabledIfSystemProperty(named = "env", matches = "staging", disabledReason = "Disabled for demo of @DisabledOnOs")
+    void thirdTest(){
+        System.out.println("This is the third test method");
+    }
+
+    @Test
+    void fourthTest(){
+        System.out.println("This is the fourth test method");
+    }
+
+    @Test
+    @DisabledIf(value = "provider", disabledReason = "Disabled for demo of @DisabledOnOs")
+    void fifthTest(){
+        System.out.println("This is the fifth test method");
+    }
+
+    boolean provider(){
+        return LocalDateTime.now().getDayOfWeek().equals(DayOfWeek.WEDNESDAY);
+    }
+}
