c09dd35e30e504d4edd0531d2ef57f678861049e
annezdz <annezdz@hotmail.com>
2021-05-24 22:18:54 -0300

    changes in AssertionsTestClass

--- src/test/java/junit5tests/AssertionsTest.java
+++ src/test/java/junit5tests/AssertionsTest.java
@@ -1,11 +1,15 @@
 package junit5tests;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertionsTest {
@@ -49,5 +53,42 @@ public class AssertionsTest {
                         "The string values were not equal"),
                 () -> assertThrows(NullPointerException.class, null),
                 () -> assertTrue(false,"This boolean condition did not evaluate to true"));
+    }
+
+    @Test
+    void assertForMapTest(){
+        Map<String, Integer> theMap = new HashMap<>();
+        theMap.put("firstKey",1);
+        theMap.put("secondKey",2);
+        theMap.put("third",3);
+
+        assertThat(theMap, Matchers.hasValue(2));
+        assertThat(theMap, Matchers.hasKey("secondKey"));
+    }
+
+    @Test
+    void assertForList(){
+        List<String> theList = Arrays.asList("firstString","secondString","thirdString");
+        assertThat(theList, Matchers.hasItem("thirdString"));
+    }
+
+    @Test
+    void assertForAnyOf(){
+        List<String> theList = Arrays.asList("firstString","secondString","thirdString");
+        assertThat(theList, Matchers.anyOf(Matchers.hasItem("thirdString"),
+                Matchers.hasItem("noString")));
+    }
+
+    @Test
+    void assertForAllOf(){
+        List<String> theList = Arrays.asList("firstString","secondString","thirdString");
+        assertThat(theList, Matchers.allOf(Matchers.hasItem("thirdString"),
+                Matchers.hasItem("noString")));
+    }
+
+    @Test
+    void assertForContainsAnyOrder(){
+        List<String> theList = Arrays.asList("firstString","secondString","thirdString");
+        assertThat(theList, Matchers.containsInAnyOrder("firstString","secondString","thirdString"));
     }
 }
